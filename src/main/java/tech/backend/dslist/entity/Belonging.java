4daf5e736aed16ff7ab39606c99845a0d439fcb7
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-09-01 10:07:57 -0300

    Domain model

---
+++ src/main/java/tech/backend/dslist/entity/Belonging.java
@@ -0,0 +1,34 @@
+package tech.backend.dslist.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer position;
+
+    public Belonging() {
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+}
